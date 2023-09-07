d4cdd329c776be10a9e3173f2e1a141832312464
Excelnoel <mannymacadex@gmail.com>
2023-09-07 13:32:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int i;
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int i;
 
-    for (i = 0; message[i]; i++) {
-        write(2, &message[i], 1);
-    }
+	for (i = 0; message[i]; i++)
+		write(2, &message[i], 1);
 
-    return 1;
+	return (1);
 }
