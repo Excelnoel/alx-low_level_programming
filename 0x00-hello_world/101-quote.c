a51cb452f405ef13f05087dee6dbcd6ce1fbd434
Excelnoel <mannymacadex@gmail.com>
2023-09-07 13:40:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,13 @@
 #include <unistd.h>
 
-/**
- * main - Entry point of the program
- *
- * Return: Always 1 (Error)
- */
 int main(void)
 {
-    char *message =
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int i;
+    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    int len = 59; // Length of the message
 
-    for (i = 0; message[i]; i++)
-        write(2, &message[i], 1);
+    if (write(STDERR_FILENO, message, len) != len) {
+        return 1;
+    }
 
-    return (1);
+    return 1;
 }
