41c403cd7d4a60d05dbaf68c558df7fe5996e1fd
Excelnoel <mannymacadex@gmail.com>
2023-09-07 13:43:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int len = sizeof(message) - 1; // Calculate the length of the message
+    int len = sizeof(message) - 1;
 
     if (write(STDERR_FILENO, message, len) != len) {
         return 1;
