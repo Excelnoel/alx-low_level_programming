7a3cdd6c5aae8caf97b5067bcd9c3d741f8aeb89
Excelnoel <mannymacadex@gmail.com>
2023-09-07 13:54:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int len = sizeof(message) - 1;
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int len = sizeof(message) - 1;
 
-    if (write(STDERR_FILENO, message, len) != len) {
-        return 1;
-    }
+	if (write(STDERR_FILENO, message, len) != len)
+	{
+		return (1);
+	}
 
-    return 1;
+	return (1);
 }
