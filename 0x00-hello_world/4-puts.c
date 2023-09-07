9709b43287b49313acc406c784b35b3d6072565e
Excelnoel <mannymacadex@gmail.com>
2023-09-07 12:41:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
-echo '#include <stdio.h>
+#include <stdio.h>
 int main() {
     puts("Programming is like building a multilingual puzzle");
     return 0;
-}' > program.c && gcc program.c -o program && ./program && echo $?
+}
