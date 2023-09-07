e046dace695204a51ee137c020ef61221199be60
Excelnoel <mannymacadex@gmail.com>
2023-09-07 13:29:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,10 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
     char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
