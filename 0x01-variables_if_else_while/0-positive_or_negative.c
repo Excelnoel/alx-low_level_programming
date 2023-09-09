e13447606f360c4ac40cfe278867c307d32aa7a8
Excelnoel <mannymacadex@gmail.com>
2023-09-09 02:55:11 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,12 +14,8 @@ int main(void)
     srand(time(NULL)); /* Seed the random number generator with the current time */
     n = rand() % 201 - 100; /* Generate a random number between -100 and 100 */
     if (n >= 0)
-    {
-        printf("%d is positive\n", n);
-    }
+      {printf("%d is positive\n", n);}
     else
-    {
-        printf("%d is negative\n", n);
-    }
+    {printf("%d is negative\n", n);}
     return (0);
 }
