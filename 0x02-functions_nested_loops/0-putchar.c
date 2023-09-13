43a9f442d857885dbbbcdf35c1dc7ecb9aa72d1b
Excelnoel <mannymacadex@gmail.com>
2023-09-12 22:11:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,5 @@
 #include "main.h"
-#include "unistd.h"
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return (0);
-}
+#include <unistd.h> // Include the necessary header for 'write'
 
 /**
  * _putchar - Writes a character to the standard output
