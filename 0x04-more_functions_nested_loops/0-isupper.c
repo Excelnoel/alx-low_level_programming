92897ebb2e9c72170277a7ed2b5469292d1e28c9
Excelnoel <mannymacadex@gmail.com>
2023-09-14 12:06:48 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: The character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return 1;
+    return 0;
+}
