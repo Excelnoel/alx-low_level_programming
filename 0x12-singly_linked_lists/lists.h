afbdbcc7a33897d765151a3b8052704a8ff3a3be
Excelnoel <mannymacadex@gmail.com>
2023-10-19 11:43:30 -0700

    list.h etc

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  *
  * struct list_s - singly linked list
@@ -17,8 +21,11 @@ typedef struct list_s
 {
         char *str;
         unsigned int len;
-        struct list s *next;
+        struct list_s *next;
 }
+
+list_t;
+
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
