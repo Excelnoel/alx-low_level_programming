8f9a37a02d80eee86c436a613a7d5c81e3c35be7
Excelnoel <mannymacadex@gmail.com>
2023-10-23 14:06:25 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,20 +3,20 @@
 #include "lists.h"
 
 /**
- * pop_listint - adds a new node at the beginning of a singly linked list.
- * @head: a linked list to print
- *
- * Return: the address of the new element, or NULLif it failed.
- */
+* pop_listint - adds a new node at the beginning of a singly linked list.
+* @head: a linked list to print
+*
+* Return: the address of the new element, or NULL if it failed.
+*/
 
 int pop_listint(listint_t **head)
 {
-	listint_t *tmp_node; /* create a new node */
+	listint_t *tmp_node; /* Create a new node */
 	int value = 0;
 
 	if (*head == NULL)
 		return (0);
-	
+
 	tmp_node = *head;
 	*head = tmp_node->next;
 	value = tmp_node->n;
