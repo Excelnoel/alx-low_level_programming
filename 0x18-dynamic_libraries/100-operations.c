b3d1aa08bc5a65b7198cde536290d8af8f18baaa
Excelnoel <mannymacadex@gmail.com>
2023-12-11 11:58:15 -0800

    100-operations.

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,101 +1,69 @@
-#include "Python.h"
+#include <stdio.h>
 
-int add(int a, int b) {
-    return a + b;
+/**
+ * add - adds two integers
+ * @i: first integer
+ * @k: second integer
+ * Return: the sum
+ */
+int add(int i, int k)
+{
+    return (i + k);
 }
 
-int sub(int a, int b) {
-    return a - b;
+/**
+ * sub - subtracts two integers
+ * @i: first integer
+ * @k: second integer
+ * Return: the difference
+ */
+int sub(int i, int k)
+{
+    return (i - k);
 }
 
-int mul(int a, int b) {
-    return a * b;
+/**
+ * mul - multiplies two integers
+ * @i: first integer
+ * @k: second integer
+ * Return: the product
+ */
+int mul(int i, int k)
+{
+    return (i * k);
 }
 
-int my_div(int a, int b) {
-    if (b != 0) {
-        return a / b;
-    } else {
-        PyErr_SetString(PyExc_ZeroDivisionError, "division by zero");
-        return 0;
+/**
+ * div - divides two integers
+ * @i: numerator
+ * @k: denominator
+ * Return: the quotient
+ */
+int div(int i, int k)
+{
+    if (k != 0)
+        return (i / k);
+    else
+    {
+        printf("Error: Division by zero\n");
+        return 0; // You might want to handle this case appropriately
     }
 }
 
-int mod(int a, int b) {
-    if (b != 0) {
-        return a % b;
-    } else {
-        PyErr_SetString(PyExc_ZeroDivisionError, "modulo by zero");
-        return 0;
+/**
+ * mod - finds the modulus of two integers
+ * @i: dividend
+ * @k: divisor
+ * Return: the modulus
+ */
+int mod(int i, int k)
+{
+    if (k != 0)
+        return (i % k);
+    else
+    {
+        printf("Error: Modulus by zero\n");
+        return 0; // You might want to handle this case appropriately
     }
 }
 
-static PyObject *wrap_add(PyObject *Py_UNUSED(self), PyObject *args) {
-    int a, b, result;
-    if (!PyArg_ParseTuple(args, "ii", &a, &b)) {
-        return NULL;
-    }
-    result = add(a, b);
-    return PyLong_FromLong(result);
-}
-
-static PyObject *wrap_sub(PyObject *Py_UNUSED(self), PyObject *args) {
-    int a, b, result;
-    if (!PyArg_ParseTuple(args, "ii", &a, &b)) {
-        return NULL;
-    }
-    result = sub(a, b);
-    return PyLong_FromLong(result);
-}
-
-static PyObject *wrap_mul(PyObject *Py_UNUSED(self), PyObject *args) {
-    int a, b, result;
-    if (!PyArg_ParseTuple(args, "ii", &a, &b)) {
-        return NULL;
-    }
-    result = mul(a, b);
-    return PyLong_FromLong(result);
-}
-
-static PyObject *wrap_div(PyObject *Py_UNUSED(self), PyObject *args) {
-    int a, b, result;
-    if (!PyArg_ParseTuple(args, "ii", &a, &b)) {
-        return NULL;
-    }
-    result = my_div(a, b);
-    return PyLong_FromLong(result);
-}
-
-static PyObject *wrap_mod(PyObject *Py_UNUSED(self), PyObject *args) {
-    int a, b, result;
-    if (!PyArg_ParseTuple(args, "ii", &a, &b)) {
-        return NULL;
-    }
-    result = mod(a, b);
-    return PyLong_FromLong(result);
-}
-
-static PyMethodDef operations_methods[] = {
-    {"add", wrap_add, METH_VARARGS, "Add two integers"},
-    {"sub", wrap_sub, METH_VARARGS, "Subtract two integers"},
-    {"mul", wrap_mul, METH_VARARGS, "Multiply two integers"},
-    {"div", wrap_div, METH_VARARGS, "Divide two integers"},
-    {"mod", wrap_mod, METH_VARARGS, "Calculate the modulo of two integers"},
-    {NULL, NULL, 0, NULL} /* Sentinel */
-};
-
-static struct PyModuleDef operations_module = {
-    PyModuleDef_HEAD_INIT,
-    "operations",
-    NULL,
-    -1,
-    operations_methods,
-    NULL, // Initialize m_slots to NULL
-    NULL,
-    NULL,
-    NULL
-};
-
-PyMODINIT_FUNC PyInit_operations(void) {
-    return PyModule_Create(&operations_module);
-}
